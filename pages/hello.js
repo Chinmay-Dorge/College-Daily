57ffbfea1842241fa1d4d0d78c4fd48464fe5b21
arushpanwar <lcs2020032@iiitl.ac.in>
2022-10-12 22:49:48 +0530

    tailwind css added

---
+++ pages/hello.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+
+const hello = () => {
+  return (
+    <div>hello
+        <Navbar />
+    </div>
+  )
+}
+
+export default hello
