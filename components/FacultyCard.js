1dc4ebd6cf10c6c2434027e40cb2425d75eee7b7
chinmay <lcs2020022@iiitl.ac.in>
2022-11-09 04:24:31 +0530

    hard code

--- components/FacultyCard.js
+++ components/FacultyCard.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import Image from 'next/image'
 
-const FacultyCard = () => {
+const FacultyCard = (props) => {
   return (
     <div>
       <div className="flex flex-col w-[300px] h-[400px] items-center justify-center bg-gray-900 rounded-lg border border-gray-200 shadow-md dark:bg-neutral-800 dark:border-neutral-800">
         <div className="flex flex-col items-center pb-10">
-          <div className="mb-4 mt-4 w-[150px] h-[150px] rounded-full shadow-lg bg-[url('https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80')] bg-cover bg-no-repeat"></div>
-          <h5 className="mb-4 text-xl font-medium text-gray-900 dark:text-white">Vishal Krishna Singh</h5>
-          <span className="text-sm text-neutral-500 dark:text-neutral-400">MultiTalented</span>
-          <span className="text-sm text-neutral-500 dark:text-neutral-400">+91-9876543210</span>
-          <span className="text-sm text-neutral-500 dark:text-neutral-400">vks@iiitl.ac.in</span>
+          <div className="mb-4 mt-4 w-[150px] h-[150px] rounded-full shadow-lg  bg-cover bg-no-repeat"><img class="cover no-repeat rounded-full" src={props.image}></img></div>
+          <h5 className="mb-4 text-xl font-medium text-gray-900 dark:text-white">{props.name}</h5>
+          <span className="text-sm text-neutral-500 dark:text-neutral-400">{props.title}</span>
+          <span className="text-sm text-neutral-500 dark:text-neutral-400">{props.no}</span>
+          <span className="text-sm text-neutral-500 dark:text-neutral-400">{props.email}</span>
           <div className="flex mt-4 space-x-3 md:mt-6">
             <div href="#" className="inline-flex items-center py-2 px-4 text-sm font-medium text-center text-gray-900 bg-white rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:bg-neutral-900 dark:text-white dark:border-neutral-900 dark:hover:bg-amber-300 dark:hover:border-amber-300 dark:focus:ring-gray-700 hover:text-neutral-900">Message</div>
           </div>
--- pages/aboutus.js
+++ pages/aboutus.js
@@ -10,12 +10,12 @@ const aboutus = () => {
         <div className={styles.innerContainer}>
             <h1>About Us</h1>
             <p className={styles.text}>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus velit ducimus, enim inventore earum, eligendi nostrum pariatur necessitatibus eius dicta a voluptates sit deleniti autem error eos totam nisi neque voluptates sit deleniti autem error eos totam nisi neque.
+              College Daily is a utility web app that will help students go through their day-to-day problems in their college life, for example in course schedule, tracking codeforces records, scheduling tasks and many more yet to come ...
             </p>
             <div className={styles.skills}>
-                <span>Web Design</span>
-                <span>Photoshop & Illustrator</span>
-                <span>Coding</span>
+                <span>!!</span>
+                <span>Software Engineering Project</span>
+                <span>!!</span>
             </div>
         </div>
         </div>
