0b7cf302d0649ba3f92d135596c49df816a6096d
arushpanwar <lcs2020032@iiitl.ac.in>
2022-11-09 03:12:58 +0530

    undermaintenance page

--- pages/codeforces.js
+++ pages/codeforces.js
@@ -1,13 +1,26 @@
-import React from 'react'
+import React,{useState} from 'react'
 import Navbar from '../components/Navbar'
 
+let codeforcesUrl = "https://codeforces.com/api/user.rating?handle=arush_panwar";
+
 const codeforces = () => {
-  return (
-    <div>
-        <Navbar />
-        track your codeforces account.
-    </div>
-  )
+    const [profile, setProfile] = React.useState();
+    fetch(codeforcesUrl)
+        .then(data => {
+            return data.json();
+        })
+        .then(post => {
+            // console.log(post.result[0]);
+            setProfile(post);
+        });
+
+
+    return (
+        <div>
+            <Navbar />
+            track your codeforces account.
+        </div>
+    )
 }
 
 export default codeforces
