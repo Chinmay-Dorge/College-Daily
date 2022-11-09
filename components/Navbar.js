f794b92c7fd8a70c65f78d8a477826cc29536d54
Varchronos <lcs2020062@iiitl.ac.in>
2022-11-09 05:31:59 +0530

    Front end in navbar and task schedule

--- components/Navbar.js
+++ components/Navbar.js
@@ -16,11 +16,11 @@ const Navbar = () => {
           </button>
         </div>
         <ul className="hidden absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2  lg:mx-auto lg:flex lg:items-center lg:w-auto lg:space-x-12">
-          <Link href='/'><li><a className="text-md text-gray-400 hover:text-amber-300" href="#">Home</a></li></Link>
-          <Link href='/taskSchedular'><li><a className="text-md text-gray-400 hover:text-amber-300" href="#">Task Schedule</a></li></Link>
-          <Link href='/codeforces'><li><a className="text-md text-gray-400 hover:text-amber-300" href="#">Track CodeForces</a></li></Link>
-          <Link href='/undermaintenance'><li><a className="text-md text-gray-400 hover:text-amber-300" href="#">About us</a></li></Link>
-          <Link href='/contactFaculty'><li><a className="text-md text-gray-400 hover:text-white" href="#" >Contact Faculty</a></li></Link>
+          <Link href='/'><li><a className="text-md text-gray-400 hover:text-amber-300 no-underline hover:text-lg ease-in-out duration-200" href="#">Home</a></li></Link>
+          <Link href='/taskSchedular'><li><a className="text-md text-gray-400 hover:text-amber-300 no-underline hover:text-lg ease-in-out duration-200" href="#">Task Schedule</a></li></Link>
+          <Link href='/codeforces'><li><a className="text-md text-gray-400 hover:text-amber-300 no-underline hover:text-lg ease-in-out duration-200" href="#">Track CodeForces</a></li></Link>
+          <Link href='/undermaintenance'><li><a className="text-md text-gray-400 hover:text-amber-300 no-underline hover:text-lg ease-in-out duration-200" href="#">About us</a></li></Link>
+          <Link href='/contactFaculty'><li><a className="text-md text-gray-400 hover:text-amber-300 no-underline hover:text-lg ease-in-out duration-200" href="#" >Contact Faculty</a></li></Link>
         </ul>
         {
           !session && status !== 'authenticated' && ( // if signed in
