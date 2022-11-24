105edf51e64341de1f579c04e7e8e63775bbc07b
Varchronos <lcs2020062@iiitl.ac.in>
2022-11-24 09:44:54 +0530

    tt update colors

--- components/TT.js
+++ components/TT.js
@@ -10,150 +10,155 @@ const TT = () => {
           <thead class="border-b">
             <tr class="flex-row">
               <th scope="col" class="text-sm font-medium text-black px-6 py-4 text-center">
-                 <button class = "w-32 py-2 bg-neutral-50 ">Day</button>
+                 <button class = "w-36 py-4 bg-red-400 ">Date</button>
               </th>
               <th scope="col" class="text-sm font-medium text-black px-6 py-4 text-center">
-              <button class = "w-32 py-2 bg-neutral-50 ">Sunday</button>
+              <button class = "w-36 py-4 bg-amber-200 ">Sunday</button>
               </th>
               <th scope="col" class="text-sm font-medium text-black px-6 py-4 text-center flex-row items-center">
-                <button class = "w-32 py-2 bg-neutral-50 ">Monday</button>
+                <button class = "w-36 py-4 bg-amber-200 ">Monday</button>
               </th>
               <th scope="col" class="text-sm font-medium text-black px-6 py-4 text-center">
-                <button class = "w-32 py-2 bg-neutral-50 ">Tuesday</button>
+                <button class = "w-36 py-4 bg-amber-200 ">Tuesday</button>
               </th>
               <th scope="col" class="text-sm font-medium text-black px-6 py-4 text-center">
-                <button class = "w-32 py-2 bg-neutral-50 ">Wednesday</button>
+                <button class = "w-36 py-4 bg-amber-200 ">Wednesday</button>
               </th>
               <th scope="col" class="text-sm font-medium text-black px-6 py-4 text-center">
-                <button class = "w-32 py-2 bg-neutral-50 ">Thursday</button>
+                <button class = "w-36 py-4 bg-amber-200 ">Thursday</button>
               </th>
               <th scope="col" class="text-sm font-medium text-black px-6 py-4 text-center">
-                <button class = "w-32 py-2 bg-neutral-50 ">Friday</button>
+                <button class = "w-36 py-4 bg-amber-200 ">Friday</button>
               </th>
               <th scope="col" class="text-sm font-medium text-black px-6 py-4 text-center">
-                <button class = "w-32 py-2 bg-neutral-50 ">Saturday</button>
+                <button class = "w-36 py-4 bg-amber-200 ">Saturday</button>
               </th>
             </tr>
           </thead>
           <tbody>
             <tr class="border-b">
-              <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-white text-center">09:00-10:00</td>
-              <td class="text-sm text-white font-light px-6 py-4 whitespace-nowrap text-center">
-                -
+              <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-black text-center">
+              <button class = "w-36 px-9 py-4 bg-amber-200 ">09:00-10:00 </button></td>
+              <td class="text-sm text-black font-light px-6 py-4 whitespace-nowrap text-center">
+              <button class = "w-36 px-3 py-4 bg-blue-200">- </button>
               </td>
-              <td class="text-sm text-white font-light px-6 py-4 whitespace-pre-line w-32 text-center">
-                Distributive/Numerical Methods
+              <td class="text-sm text-black font-light px-6 py-4 whitespace-pre-line text-center">
+              <button class = "w-36 px-3 py-2 bg-blue-200">Distributied Methods </button>
               </td>
-              <td class="text-sm text-white font-light px-6 py-4 whitespace-pre-line w-32 text-center">
-                Distributive/Numerical Methods
+              <td class="text-sm text-black font-light px-6 py-4 whitespace-pre-line  text-center">
+              <button class = "w-36 px-3 py-2 bg-blue-200">Distributied Methods </button>
               </td>
-              <td class="text-sm text-white font-light px-6 py-4 whitespace-pre-line w-32 text-center">
-                Distributive/Numerical Methods
+              <td class="text-sm text-black font-light px-6 py-4 whitespace-pre-line text-center">
+              <button class = "w-36 px-3 py-2 bg-blue-200">Distributied Methods </button>
               </td>
-              <td class="text-sm text-white font-light px-6 py-4 whitespace-pre-line w-32 text-center">
-                Distributive/Numerical Methods
+              <td class="text-sm text-black font-light px-6 py-4 whitespace-pre-line  text-center">
+              <button class = "w-36 px-3 py-2 bg-blue-200">Distributied Methods </button>
               </td>
-              <td class="text-sm text-white font-light px-6 py-4 whitespace-pre-line w-32 text-center">
-                -
+              <td class="text-sm text-black font-light px-6 py-4 whitespace-pre-line  text-center">
+              <button class = "w-36 py-4 bg-blue-200">-</button>
               </td>
-              <td class="text-sm text-white font-light px-6 py-4 whitespace-pre-line w-32 text-center">
-                -
-              </td>
-            </tr>
-            <tr class="border-b">
-              <td class="px-6 py-4 whitespace-pre-line w-32 text-sm font-medium text-white text-center">10:00-11:00</td>
-              <td class="text-sm text-white font-light px-6 py-4 whitespace-pre-line w-32 text-center">
-                -
-              </td>
-              <td class="text-sm text-white font-light px-6 py-4 whitespace-pre-line w-32 text-center">
-                Software Engineering
-              </td>
-              <td class="text-sm text-white font-light px-6 py-4 whitespace-pre-line w-32 text-center">
-                Software Engineering
-              </td>
-              <td class="text-sm text-white font-light px-6 py-4 whitespace-pre-line w-32 text-center">
-                Software Engineering
-              </td>
-              <td class="text-sm text-white font-light px-6 py-4 whitespace-pre-line w-32 text-center">
-                -
-              </td>
-              <td class="text-sm text-white font-light px-6 py-4 whitespace-pre-line w-32 text-center">
-                -
-              </td>
-              <td class="text-sm text-white font-light px-6 py-4 whitespace-pre-line w-32 text-center">
-                -
+              <td class="text-sm text-black font-light px-6 py-4 whitespace-pre-line  text-center">
+              <button class = "w-36 py-4 bg-blue-200">- </button>
               </td>
             </tr>
             <tr class="border-b">
-              <td class="px-6 py-4 whitespace-pre-line w-32 text-sm font-medium text-white text-center">11:15-12:15</td>
-              <td class="text-sm text-white font-light px-6 py-4 whitespace-pre-line w-32 text-center">
-                -
+              <td class="px-6 py-4 whitespace-pre-line w-36 text-sm font-medium text-black text-center">
+              <button class = "w-36  px-9 py-4 bg-amber-200 ">10:00-11:00</button></td>
+              <td class="text-sm text-black font-light px-6 py-4 whitespace-pre-line  text-center">
+              <button class = "w-36 py-4 bg-blue-200 ">- </button>
               </td>
-              <td class="text-sm text-white font-light px-6 py-4 whitespace-pre-line w-32 text-center">
-                Compiler Design
+              <td class="text-sm text-black font-light px-6 py-4 whitespace-pre-line  text-center">
+              <button class = "w-36 px-3 py-2 bg-blue-200 ">Software Engineering</button>
               </td>
-              <td class="text-sm text-white font-light px-6 py-4 whitespace-pre-line w-32 text-center">
-                Compiler Design
+              <td class="text-sm text-black font-light px-6 py-4 whitespace-pre-line  text-center">
+              <button class = "w-36 px-3 py-2 bg-blue-200 ">Software Engineering</button>
               </td>
-              <td class="text-sm text-white font-light px-6 py-4 whitespace-pre-line w-32 text-center">
-                Compiler Design
+              <td class="text-sm text-black font-light px-6 py-4 whitespace-pre-line text-center">
+              <button class = "w-36 px-3 py-2 bg-blue-200 ">Software Engineering</button>
               </td>
-              <td class="text-sm text-white font-light px-6 py-4 whitespace-pre-line w-32 text-center">
-                Compiler Design
+              <td class="text-sm text-black font-light px-6 py-4 whitespace-pre-line text-center">
+              <button class = "w-36 px-3 py-4 bg-blue-200 ">-</button>
               </td>
-              <td class="text-sm text-white font-light px-6 py-4 whitespace-pre-line w-32 text-center">
-                -
+              <td class="text-sm text-black font-light px-6 py-4 whitespace-pre-line text-center">
+              <button class = "w-36 px-3 py-4 bg-blue-200 ">-</button>
               </td>
-              <td class="text-sm text-white font-light px-6 py-4 whitespace-pre-line w-32 text-center">
-                -
-              </td>
-            </tr>
-            <tr class="border-b">
-              <td class="px-6 py-4 whitespace-pre-line w-32 text-sm font-medium text-white text-center">12:15-13:15</td>
-              <td class="text-sm text-white font-light px-6 py-4 whitespace-pre-line w-32 text-center">
-                -
-              </td>
-              <td class="text-sm text-white font-light px-6 py-4 whitespace-pre-line w-32 text-center">
-                Cryptography
-              </td>
-              <td class="text-sm text-white font-light px-6 py-4 whitespace-pre-line w-32 text-center">
-                Cryptography
-              </td>
-              <td class="text-sm text-white font-light px-6 py-4 whitespace-pre-line w-32 text-center">
-                Cryptography
-              </td>
-              <td class="text-sm text-white font-light px-6 py-4 whitespace-pre-line w-32 text-center">
-                -
-              </td>
-              <td class="text-sm text-white font-light px-6 py-4 whitespace-pre-line w-32 text-center">
-                -
-              </td>
-              <td class="text-sm text-white font-light px-6 py-4 whitespace-pre-line w-32 text-center">
-                -
+              <td class="text-sm text-black font-light px-6 py-4 whitespace-pre-line text-center">
+              <button class = "w-36 px-3 py-4 bg-blue-200 ">-</button>
               </td>
             </tr>
             <tr class="border-b">
-              <td class="px-6 py-4 whitespace-pre-line w-32 text-sm font-medium text-white text-center">14:00-15:00</td>
-              <td class="text-sm text-white font-light px-6 py-4 whitespace-pre-line w-32 text-center">
-                -
+              <td class="px-6 py-4 whitespace-pre-line w-36 text-sm font-medium text-black text-center">
+              <button class = "w-36  px-9 py-4 bg-amber-200 ">10:00-11:00</button></td>
+              <td class="text-sm text-black font-light px-6 py-4 whitespace-pre-line text-center">
+              <button class = "w-36 py-4 bg-blue-200 ">- </button>
               </td>
-              <td class="text-sm text-white font-light px-6 py-4 whitespace-pre-line w-32 text-center">
-                Computer Graphics
+              <td class="text-sm text-black font-light px-6 py-4 whitespace-pre-line  text-center">
+              <button class = "w-36 py-4 bg-blue-200 ">Compiler Design</button>
               </td>
-              <td class="text-sm text-white font-light px-6 py-4 whitespace-pre-line w-32 text-center">
-                Computer Graphics
+              <td class="text-sm text-black font-light px-6 py-4 whitespace-pre-line text-center">
+              <button class = "w-36 py-4 bg-blue-200 ">Compiler Design</button>
               </td>
-              <td class="text-sm text-white font-light px-6 py-4 whitespace-pre-line w-32 text-center">
-                Computer Graphics
+              <td class="text-sm text-black font-light px-6 py-4 whitespace-pre-line text-center">
+              <button class = "w-36 py-4 bg-blue-200 ">Compiler Design</button>
               </td>
-              <td class="text-sm text-white font-light px-6 py-4 whitespace-pre-line w-32 text-center">
-                -
+              <td class="text-sm text-black font-light px-6 py-4 whitespace-pre-line text-center">
+              <button class = "w-36 py-4 bg-blue-200 ">Compiler Design</button>
               </td>
-              <td class="text-sm text-white font-light px-6 py-4 whitespace-pre-line w-32 text-center">
-                -
+              <td class="text-sm text-black font-light px-6 py-4 whitespace-pre-line text-center">
+              <button class = "w-36 py-4 bg-blue-200 ">- </button>
               </td>
-              <td class="text-sm text-white font-light px-6 py-4 whitespace-pre-line w-32 text-center">
-                -
+              <td class="text-sm text-black font-light px-6 py-4 whitespace-pre-line text-center">
+              <button class = "w-36 py-4 bg-blue-200 ">- </button>
+              </td>
+            </tr>
+            <tr class="border-b">
+              <td class="px-6 py-4 whitespace-pre-line w-36 text-sm font-medium text-black text-center">
+              <button class = "w-36  px-9 py-4 bg-amber-200 ">12:15-13:15</button></td>
+              <td class="text-sm text-black font-light px-6 py-4 whitespace-pre-line text-center">
+              <button class = "w-36 py-4 bg-blue-200 ">- </button>
+              </td>
+              <td class="text-sm text-black font-light px-6 py-4 whitespace-pre-line text-center">
+              <button class = "w-36 py-4 bg-blue-200 ">Cryptography</button>
+              </td>
+              <td class="text-sm text-black font-light px-6 py-4 whitespace-pre-line text-center">
+              <button class = "w-36 py-4 bg-blue-200 ">Cryptography</button>
+              </td>
+              <td class="text-sm text-black font-light px-6 py-4 whitespace-pre-line text-center">
+              <button class = "w-36 py-4 bg-blue-200 ">Cryptography</button>
+              </td>
+              <td class="text-sm text-black font-light px-6 py-4 whitespace-pre-line text-center">
+              <button class = "w-36 py-4 bg-blue-200 ">- </button>
+              </td>
+              <td class="text-sm text-black font-light px-6 py-4 whitespace-pre-line text-center">
+              <button class = "w-36 py-4 bg-blue-200 ">- </button>
+              </td>
+              <td class="text-sm text-black font-light px-6 py-4 whitespace-pre-line text-center">
+              <button class = "w-36 py-4 bg-blue-200 ">- </button>
+              </td>
+            </tr>
+            <tr class="border-b">
+              <td class="px-6 py-4 whitespace-pre-line w-36 text-sm font-medium text-black text-center">
+              <button class = "w-36 px-9 py-4 bg-amber-200 ">14:00-15:00</button></td>
+              <td class="text-sm text-white font-light px-6 py-4 whitespace-pre-line text-center">
+              <button class = "w-36 py-4 bg-blue-200 ">- </button>
+              </td>
+              <td class="text-sm text-black font-light px-6 py-4 whitespace-pre-line text-center">
+              <button class = "w-36 px-3 py-4 bg-blue-200 ">Computer Graphics</button>
+              </td>
+              <td class="text-sm text-black font-light px-6 py-4 whitespace-pre-line text-center">
+              <button class = "w-36 px-3 py-4 bg-blue-200 ">Computer Graphics</button>
+              </td>
+              <td class="text-sm text-black font-light px-6 py-4 whitespace-pre-line text-center">
+              <button class = "w-36 px-3 py-4 bg-blue-200 ">Computer Graphics</button>
+              </td>
+              <td class="text-sm text-black font-light px-6 py-4 whitespace-pre-line text-center">
+              <button class = "w-36 py-4 bg-blue-200 ">- </button>
+              </td>
+              <td class="text-sm text-black font-light px-6 py-4 whitespace-pre-line  text-center">
+              <button class = "w-36 py-4 bg-blue-200 ">- </button>
+              </td>
+              <td class="text-sm text-black font-light px-6 py-4 whitespace-pre-line  text-center">
+              <button class = "w-36 py-4 bg-blue-200 ">- </button>
               </td>
             </tr>
           </tbody>
